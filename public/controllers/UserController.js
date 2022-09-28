a38a9a4a3f4480fb10e70e7859d2682b0521aa31
HenriqueFranca <henriqsf@gmail.com>
2022-09-28 10:50:47 -0300

    Ajax com Xml

--- public/controllers/UserController.js
+++ public/controllers/UserController.js
@@ -199,9 +199,26 @@ class UserController {
 
     selectAll(){
 
-        let users = User.getUsersStorage();
+       // let users = User.getUsersStorage();
+       let ajax = new XMLHttpRequest();
 
-        users.forEach(dataUser=>{
+       ajax.open('GET', '/users');
+
+       ajax.onload = event =>{
+
+        let obj = { users:[] };
+        
+        try{
+
+             obj =  JSON.parse(ajax.responseText);
+
+        }catch(e){
+
+            console.error(e);
+
+        }        
+
+        obj.users.forEach(dataUser=>{
 
             let user = new User();
 
@@ -210,6 +227,10 @@ class UserController {
             this.addLine(user);
 
         });
+
+       };
+
+       ajax.send();
 
     }
 
